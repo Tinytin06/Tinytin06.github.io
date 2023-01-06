c897337654c95a7245c7bb1ad519456ed0cd4a36
Tinytin06 <52684983+Tinytin06@users.noreply.github.com>
2023-01-05 23:14:56 -0800

    Update square.js

--- square.js
+++ square.js
@@ -1,12 +1,13 @@
 class square{
     constructor(game){
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./spritesheet (1).png"),0 , 0, 1200, 1200, 6, 0.2 ,1);
+        //this.animator = new Animator(ASSET_MANAGER.getAsset("./spritesheet (1).png"),0 , 0, 1200, 1200, 6, 0.2 ,1);
     };
     update(){
 
     };
     draw(ctx){
-        this.animator.drawFrame(this.game.clockTick, ctx, 10, 10, 480, 60);
+        ctx.drawImage(ASSET_MANAGER.getAsset("./spritesheet (1).png"),0 ,0, 240, 20);
+        //this.animator.drawFrame(this.game.clockTick, ctx, 10, 10, 480, 60);
     };
 }
